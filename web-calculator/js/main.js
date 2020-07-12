2a6bf756cac85596aec90ce23c76aafe8bae7170
SimonBuusJensen <Bringuer6194!>
2020-07-12 22:44:34 +0200

    Created JS functions for each type of button on the calculator.

--- web-calculator/js/main.js
+++ web-calculator/js/main.js
@@ -1,4 +1,33 @@
 
-function on_button_click() {
-  document.getElementById("calculater-display").innerText = "7";
+function on_number_button_click(input) {
+  document.getElementById("calculator-expression").value += input;
 }
+
+function on_start_paranthese_button_click(input) {
+  document.getElementById("calculator-expression").value += input;
+}
+
+function on_end_paranthese_button_click(input) {
+  var expression = document.getElementById("calculator-expression").value;
+  if (expression.includes("(")) {
+    document.getElementById("calculator-expression").value += input;
+  }
+}
+
+function on_sign_operator_click(input) {
+  document.getElementById("calculator-expression").value += input;
+}
+
+function on_delete_button_click() {
+  document.getElementById("calculator-expression").value = "";
+}
+
+function on_dot_button_click() {
+  return 0
+}
+
+function on_equals_button_click() {
+  return 0
+}
+
+
